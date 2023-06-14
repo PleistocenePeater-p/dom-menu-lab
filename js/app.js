3d9df0d9de3e16ce3b036321859eed10812c73b7
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-06-14 15:21:47 -0700

    revised, ty Shay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-//global variable, Part 2: Task 5.1
-var showingSubMenu = false;
 
-// Menu data structure
+
+// Menu data structure, Task 5.0
 const menuLinks = [
     {text: 'about', href: '/about'},
     {text: 'catalog', href: '#', subLinks: [
@@ -21,30 +20,69 @@ const menuLinks = [
   ];
   
 
+//Task 1.0
 var mainEl = document.querySelector("main");
+//Task 1.1
 mainEl.style.backgroundColor = "var(--main-bg)";
+//Task 1.2
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+//Task 1.3
 mainEl.classList.add("flex-ctr");
-
-var topMenuEl = document.getElementById("top-menu");
-//topMenuEl.style.height = "100%";
-topMenuEl.setAttribute("style", "height:100%")
-// ref https://www.w3schools.com/jsref/prop_style_height.asp
+//Task 2.0
+const topMenuEl = document.getElementById("top-menu");
+//Task 2.1
+topMenuEl.style.height = "100%"
+//Task 2.2
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+//Task 2.3
 topMenuEl.classList.add("flex-around");
 
+//Task 3.1 
+menuLinks.forEach(function(link){
+    const linksEl = document.createElement("a");
+    linksEl.setAttribute("href", link.href);
+    linksEl.textContent = link.text;
+    topMenuEl.appendChild(linksEl)
+// ref https://www.w3schools.com/jsref/prop_style_height.asp
+// helpful https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+//thank god https://www.w3schools.com/jsref/prop_node_textcontent.asp
+//thank god(2) https://www.w3schools.com/jsref/prop_option_text.asp
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/append is this useful? 
+//append child seems better
+});
+
 //======Part 2======//
+  //Task 4.0
+  const subMenuEl = document.getElementById("sub-menu"); 
+  //Task 4.1
+  //subMenuEl.setAttribute("style.height:100%");  
+  subMenuEl.style.height = "100%";
+  //Task 4.2
+  subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"; 
+  //Task 4.3
+  subMenuEl.classList.add("flex-around"); 
+  //Task 4.4
+  subMenuEl.style.position = "absolute";  
+  //Task 4.5
+  subMenuEl.style.top = "0";  
+
+//Task 5.1
+const topMenuLinks = document.querySelectorAll("#top-menu A");
+let showingSubMenu = false;
+
 //Task 5.2 - Attach a delegated 'click' event listener to topMenuEl.
 topMenuEl.addEventListener("click", function(evt){
     evt.preventDefault()
-    let lmnt = evt.target; 
-//https://www.w3schools.com/jsref/event_target.asp
-// "Get the element where the event occured: const element = event.target;"
+    const lmnt = evt.target; 
     if (lmnt.tagName !== "A")
         return;
     console.log(lmnt.textContent); 
+
+//https://www.w3schools.com/jsref/event_target.asp
+// "Get the element where the event occured: const element = event.target;"
 //https://www.w3schools.com/jsref/prop_node_textcontent.asp
 //"Return the text content of an element: let text = element.textContent"
+
 // Task 5.3
     if (lmnt.classList.contains("active")){
         lmnt.classList.remove("active");
@@ -67,13 +105,13 @@ const lmntData = menuLinks.find(function(lmntObj){
 showingSubMenu = "subLinks" in lmntData;
 
 //Task 5.7
-if (showingSubMenu = true){
+if (showingSubMenu){
   buildSubMenu(lmntData.subLinks);
   subMenuEl.style.top = "100%"
 }
 else {
   subMenuEl.style.top = "0"
-  mainEl.innerHTML = "<h1>about</h1>"
+  mainEl.innerHTML = "<h1>about</h1>";
   }
 });
 
@@ -82,8 +120,8 @@ function buildSubMenu(subLinks){
   subMenuEl.innerHTML = "";
   subLinks.forEach(function(lmnt){
     const linksEl = document.createElement("A")
-    linksEl.setAttribute("href", link.href);
-    linksEl.textContent = link.text;
+    linksEl.setAttribute("href", lmnt.href); //
+    linksEl.textContent = lmnt.text; //
     subMenuEl.appendChild(linksEl);
   });
 }
@@ -107,36 +145,13 @@ subMenuEl.addEventListener("click", function(evt){
   });
 
   //Task 6.3
-  mainEl.innerHTML = "<h1>${link.textcontent}</h1>";
+  mainEl.innerHTML = `<h1>${link.textcontent}</h1>`;
 
 });
 
 
-//3 
-menuLinks.forEach(function(link){
-    var linksEl = document.createElement("a");
-// helpful https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-    linksEl.setAttribute("href", link.href)
-    linksEl.textContent = link.text
-//thank god https://www.w3schools.com/jsref/prop_node_textcontent.asp
-//thank god(2) https://www.w3schools.com/jsref/prop_option_text.asp
-    topMenuEl.appendChild(linksEl)
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/append is this useful? 
-//append child seems better
-});
 
 
-/*
-  ==========Part 2==========
-  */
 
-  const subMenuEl = document.getElementById("sub-menu");
-  subMenuEl.setAttribute("style", "height:100%")
-  subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-  subMenuEl.classList.add("flex-around");
-  subMenuEl.style.position = "absolute";
-  subMenuEl.style.top = "0";
-
-const topMenuLinks = document.querySelectorAll("a")
 
 
